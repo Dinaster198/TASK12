c0a288d181b7eb5c1d83b4c59e24bf3c331020e8
Jose Alejandro Salinas Alves <sksrteiopc@gmail.com>
2026-09-25 11:27:09 +0000

    Portofilio
    Add "m"

--- task1portoflio.py
+++ task1portoflio.py
@@ -2,4 +2,4 @@ name = "jose alejandro salinas alves"
 age = 19
 height = 1.90
 is_student = True
-print(f"{name} | {age} | {height} | {is_student}")
+print(f"{name} | {age} | {height} | {is_student}"m)
