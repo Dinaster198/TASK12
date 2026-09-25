4966744c933736ab7c3c840ae2bd3faf0dda464e
Jose Alejandro Salinas Alves <sksrteiopc@gmail.com>
2026-09-25 12:34:31 +0000

    Parte1 y 2
    .

---
+++ PortfolioTask2.py
@@ -0,0 +1,17 @@
+numero = 2
+decimal = 2.3
+palabra = "hola"
+booleana= True
+resultado = None
+
+print(f"{numero}  {type(numero)}")
+
+print(f"{decimal}  {type(decimal)}")
+
+print(f"{palabra}  {type(palabra)}")
+
+print(f"{booleana}  {type(booleana)}")
+
+print(f"{resultado}  {type(resultado)}")
+
+
